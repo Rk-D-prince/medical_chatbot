c2a9eac8dc51f551f07a2e554817ce6235acb15b
Rk-D-prince <126610387+Rk-D-prince@users.noreply.github.com>
2025-04-20 11:56:53 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Rajakannan',
+    author_email= 'rk9102001@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
